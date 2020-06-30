7a143f32dd2faa4784a55233efaad062846b8a50
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-30 11:35:21 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/VueMathPage/'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/',
     outputDir: 'docs',
     assetsDir: './',
